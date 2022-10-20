b46a597b4e2dde7d505dbacba1acc3758dcbe316
valkyrie_pilot <valk@randomairborne.dev>
2022-10-20 17:29:07 +0000

    fix an error

--- slash/src/handler.rs
+++ slash/src/handler.rs
@@ -16,15 +16,17 @@ pub async fn handle(
     crate::discord_sig_validation::validate_discord_sig(&headers, &body, &state.pubkey)?;
     // TODO make this actually do something
     let interaction: Interaction = serde_json::from_slice(&body)?;
-    Ok(Json(crate::processor::process(interaction)?))
+    Ok(Json(crate::processor::process(interaction, state)?))
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum HandlerError {
     #[error("Signature validation error: {0}")]
-    ValidationError(#[from] crate::discord_sig_validation::SignatureValidationError),
+    Validation(#[from] crate::discord_sig_validation::SignatureValidationError),
     #[error("serde_json validation error: {0}")]
-    SerdeJsonError(#[from] serde_json::Error),
+    SerdeJson(#[from] serde_json::Error),
+    #[error("Command processor error: {0}")]
+    CmdProcessor(#[from] crate::processor::CommandProcessorError),
 }
 
 impl IntoResponse for HandlerError {
--- slash/src/processor.rs
+++ slash/src/processor.rs
@@ -1,14 +1,18 @@
+use crate::AppState;
 use twilight_model::{
     application::interaction::{Interaction, InteractionType},
     http::interaction::{InteractionResponse, InteractionResponseData, InteractionResponseType},
 };
 use twilight_util::builder::InteractionResponseDataBuilder;
 
-pub fn process(interaction: Interaction) -> Result<InteractionResponse, CommandProcessorError> {
+pub fn process(
+    interaction: Interaction,
+    state: AppState,
+) -> Result<InteractionResponse, CommandProcessorError> {
     Ok(if interaction.kind == InteractionType::ApplicationCommand {
         InteractionResponse {
             kind: InteractionResponseType::ChannelMessageWithSource,
-            data: Some(process_app_cmd(interaction)?),
+            data: Some(process_app_cmd(interaction, state)?),
         }
     } else {
         InteractionResponse {
@@ -20,6 +24,7 @@ pub fn process(interaction: Interaction) -> Result<InteractionResponse, CommandP
 
 fn process_app_cmd(
     interaction: Interaction,
+    state: AppState,
 ) -> Result<InteractionResponseData, CommandProcessorError> {
     Ok(InteractionResponseDataBuilder::new().build())
 }
