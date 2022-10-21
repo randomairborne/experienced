002690e29fe8c94f985f8076606b159f19106a83
valkyrie_pilot <valk@randomairborne.dev>
2022-10-20 20:53:25 -0600

    Add some comments and reworks

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -3,19 +3,23 @@
 use futures::stream::StreamExt;
 use rand::Rng;
 use sqlx::MySqlPool;
-use std::{collections::HashSet, env, error::Error, sync::Arc};
-use tokio::sync::Mutex;
+use std::{env, error::Error, sync::Arc};
 use twilight_gateway::{
     cluster::{Cluster, ShardScheme},
     Event, Intents,
 };
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn main() {
     dotenvy::dotenv().ok();
-    let token = env::var("DISCORD_TOKEN").expect("Failed to get DATABASE_TOKEN environment variable");
+    let token =
+        env::var("DISCORD_TOKEN").expect("Failed to get DATABASE_TOKEN environment variable");
     let mysql = env::var("DATABASE_URL").expect("Failed to get DATABASE_URL environment variable");
-
+    let db = sqlx::mysql::MySqlPoolOptions::new()
+        .max_connections(50)
+        .connect(&mysql)
+        .await
+        .expect("Failed to connect to database");
     let scheme = ShardScheme::Range {
         from: 0,
         to: 0,
@@ -27,7 +31,8 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let (cluster, mut events) = Cluster::builder(token.clone(), intents)
         .shard_scheme(scheme)
         .build()
-        .await?;
+        .await
+        .expect("Failed to create discord cluster");
 
     let cluster = Arc::new(cluster);
 
@@ -36,55 +41,30 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     tokio::spawn(async move {
         cluster_spawn.up().await;
     });
-    let users: UserList = Arc::new(Mutex::new(HashSet::new()));
-    let db = sqlx::mysql::MySqlPoolOptions::new()
-        .max_connections(50)
-        .connect(&mysql)
-        .await?;
-    tokio::spawn(flush(db, users.clone()));
+
     while let Some((_shard_id, event)) = events.next().await {
-        tokio::spawn(handle_event(event, users.clone()));
+        tokio::spawn(handle_event(event, db.clone()));
     }
-
-    Ok(())
 }
 
-async fn handle_event(event: Event, users: UserList) {
+async fn handle_event(event: Event, db: MySqlPool) {
     if let Event::MessageCreate(msg) = event {
-        if msg.author.bot {
-        } else {
-            let mut u = users.lock().await;
-            u.insert(msg.author.id.to_string());
-        }
-    }
-}
-
-type UserList = Arc<Mutex<HashSet<String>>>;
-
-async fn flush(db: MySqlPool, users: UserList) {
-    loop {
-        let mut users_locked = users.lock().await;
-        let actives = users_locked.clone();
-        users_locked.clear();
-        drop(users_locked);
-        tokio::spawn(do_insert(db.clone(), actives));
-        tokio::time::sleep(std::time::Duration::from_secs(60)).await;
-    }
-}
-
-async fn do_insert(db: MySqlPool, users: HashSet<String>) {
-    for user in users {
-        let db = db.clone();
-        tokio::spawn(async move {
+        if !msg.author.bot {
+            // TODO edit sql to only allow user to gain xp one time per minute
+            // This could be done using redis, but I don't really want to add more external requirements.
+            // I plan to somehow do it within the DB, prob with an additional column.
             let xp_count = rand::thread_rng().gen_range(15..=25);
-            if let Err(e) = sqlx::query("INSERT INTO levels (id, xp) VALUES (?, ?) ON DUPLICATE KEY UPDATE xp=xp+?")
-            .bind(user)
+            if let Err(e) = sqlx::query(
+                "INSERT INTO levels (id, xp) VALUES (?, ?) ON DUPLICATE KEY UPDATE xp=xp+?",
+            )
+            .bind(msg.author.id.to_string())
             .bind(xp_count)
             .bind(xp_count)
             .execute(&db)
-            .await {
+            .await
+            {
                 eprintln!("SQL insert error: {e:?}");
             };
-        });
+        }
     }
 }
