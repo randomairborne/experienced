6216d4444118440b63cdd528715b1322b9a89c20
valkyrie_pilot <valk@randomairborne.dev>
2022-10-24 18:48:21 +0000

    Add autorole to listener

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -9,10 +9,7 @@ use twilight_gateway::{
     cluster::{Cluster, ShardScheme},
     Event, Intents,
 };
-use twilight_model::id::{
-    marker::{RoleMarker, UserMarker},
-    Id,
-};
+use twilight_model::id::{marker::UserMarker, Id};
 
 #[tokio::main]
 async fn main() {
@@ -25,22 +22,11 @@ async fn main() {
         .connect(&mysql)
         .await
         .expect("Failed to connect to database");
-    let rewards: Vec<(u64, Id<RoleMarker>)> = query!("SELECT id, requirement FROM role_rewards",)
-        .fetch_all(&db)
-        .await
-        .expect("Failed to get role rewards from database")
-        .iter()
-        .map(|v| {
-            (
-                v.requirement,
-                Id::<RoleMarker>::new_checked(v.id)
-                    .expect("One of your role rewards has an invalid role ID!")
-                    .cast(),
-            )
-        })
-        .collect::<Vec<(u64, Id<RoleMarker>)>>()
-        .sort();
+
     let cooldown: Arc<DashMap<Id<UserMarker>, Instant>> = Arc::new(DashMap::new());
+
+    let client = Arc::new(twilight_http::Client::new(token.clone()));
+
     let scheme = ShardScheme::Range {
         from: 0,
         to: 0,
@@ -70,7 +56,7 @@ async fn main() {
             event,
             db.clone(),
             cooldown.clone(),
-            rewards.clone(),
+            client.clone(),
         ));
     }
 }
@@ -79,36 +65,52 @@ async fn handle_event(
     event: Event,
     db: MySqlPool,
     cooldown: Arc<DashMap<Id<UserMarker>, Instant>>,
-    rewards: HashMap<u64, Vec<(u64, Id<RoleMarker>)>>,
+    http: Arc<twilight_http::Client>,
 ) {
     if let Event::MessageCreate(msg) = event {
-        if !msg.author.bot && cooldown.get(&msg.author.id).is_none() {
-            let xp_count = rand::thread_rng().gen_range(15..=25);
-            if let Err(e) = query!(
-                "INSERT INTO levels (id, xp) VALUES (?, ?) ON DUPLICATE KEY UPDATE xp=xp+?",
-                msg.author.id.get(),
-                xp_count,
-                xp_count
-            )
-            .execute(&db)
-            .await
-            {
-                eprintln!("SQL insert error: {e:?}");
-            };
-            cooldown.insert(msg.author.id, Instant::now());
-            let xp = match query!("SELECT xp FROM levels WHERE id = ?", msg.author.id.get())
-                .fetch_one(&db)
+        if let Some(guild_id) = msg.guild_id {
+            if !msg.author.bot && cooldown.get(&msg.author.id).is_none() {
+                let xp_count = rand::thread_rng().gen_range(15..=25);
+                if let Err(e) = query!(
+                    "INSERT INTO levels (id, xp) VALUES (?, ?) ON DUPLICATE KEY UPDATE xp=xp+?",
+                    msg.author.id.get(),
+                    xp_count,
+                    xp_count
+                )
+                .execute(&db)
                 .await
-            {
-                Ok(xp) => xp,
-                Err(e) => {
-                    eprintln!("SQL select error: {e:?}");
-                    return;
+                {
+                    eprintln!("SQL insert error: {e:?}");
+                };
+                cooldown.insert(msg.author.id, Instant::now());
+                let xp = match query!("SELECT xp FROM levels WHERE id = ?", msg.author.id.get())
+                    .fetch_one(&db)
+                    .await
+                {
+                    Ok(xp) => xp,
+                    Err(e) => {
+                        eprintln!("SQL select error: {e:?}");
+                        return;
+                    }
                 }
+                .xp;
+                let level_info = mee6::LevelInfo::new(xp);
+                let reward = match query!("SELECT id FROM role_rewards WHERE guild = ? AND requirement >= ? ORDER BY requirement DESC LIMIT 1", guild_id.get(), level_info.level())
+                    .fetch_one(&db)
+                    .await
+                {
+                    Ok(rw) => rw.id,
+                    Err(e) => {
+                        if let sqlx::Error::RowNotFound = e {return;}
+                        eprintln!("SQL select error: {e:?}");
+                        return;
+                    }
+                };
+                http.add_guild_member_role(guild_id, msg.author.id, Id::new(reward))
+                    .exec()
+                    .await
+                    .ok();
             }
-            .xp;
-            let level_info = mee6::LevelInfo::new(xp);
-            for reward in rewards {}
         }
     }
 }
