dc0cb22ab1384a926c8d1d37371ebda23a5eece3
valkyrie_pilot <valk@randomairborne.dev>
2023-01-07 09:41:00 -0700

    Fix clippy warnings

--- slash/src/render_card.rs
+++ slash/src/render_card.rs
@@ -18,17 +18,17 @@ pub async fn render(
         name,
         discriminator,
     };
-    tokio::task::spawn_blocking(|| do_render(context)).await?
+    tokio::task::spawn_blocking(|| do_render(&context)).await?
 }
 
-fn do_render(context: Context) -> Result<Vec<u8>, RenderingError> {
+fn do_render(context: &Context) -> Result<Vec<u8>, RenderingError> {
     let opt = resvg::usvg::Options::default();
     let mut fontdb = fontdb::Database::new();
     fontdb.load_font_data(include_bytes!("resources/OpenSans.ttf").to_vec());
     let mut tt = tinytemplate::TinyTemplate::new();
     tt.add_template("svg", include_str!("resources/card.svg"));
 
-    let svg = tt.render("hello", &context)?;
+    let svg = tt.render("hello", context)?;
     let tree = resvg::usvg::Tree::from_str(&svg, &opt)?;
     let pixmap_size = tree.size.to_screen_size();
     let mut pixmap = resvg::tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height())
