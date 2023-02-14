69ca8a2749ca311aca3b8e9650002b6994706516
valkyrie_pilot <valk@randomairborne.dev>
2023-02-13 18:48:02 -0700

    improve message processing time

--- listener/src/message.rs
+++ listener/src/message.rs
@@ -14,26 +14,17 @@ pub async fn save(
         let has_sent_key = format!("cooldown-{guild_id}-{}", msg.author.id);
         let has_sent: bool = redis.get(&has_sent_key).await?;
         if !msg.author.bot && !has_sent {
-            let xp_count = rand::thread_rng().gen_range(15..=25);
-            #[allow(clippy::cast_possible_wrap)]
-            query!(
-                "INSERT INTO levels (id, xp, guild) VALUES ($1, $2, $3) ON CONFLICT (id, guild) DO UPDATE SET xp=levels.xp+excluded.xp",
-                msg.author.id.get() as i64,
-                i64::from(xp_count),
-                guild_id.get() as i64
-            )
-            .execute(&db)
-            .await?;
-            redis.set_ex(&has_sent_key, true, 60).await?;
+            let xp_count: i64 = rand::thread_rng().gen_range(15..=25);
             #[allow(clippy::cast_possible_wrap, clippy::cast_sign_loss)]
             let xp = query!(
-                "SELECT xp FROM levels WHERE id = $1 AND guild = $2",
+                "INSERT INTO levels (id, xp, guild) VALUES ($1, $2, $3) ON CONFLICT (id, guild) DO UPDATE SET xp=levels.xp+excluded.xp RETURNING xp",
                 msg.author.id.get() as i64,
+                xp_count,
                 guild_id.get() as i64
             )
             .fetch_one(&db)
-            .await?
-            .xp as u64;
+            .await?.xp as u64;
+            redis.set_ex(&has_sent_key, true, 60).await?;
             let level_info = mee6::LevelInfo::new(xp);
             #[allow(clippy::cast_sign_loss, clippy::cast_possible_wrap)]
             let reward = query!("SELECT id FROM role_rewards WHERE guild = $1 AND requirement <= $2 ORDER BY requirement DESC LIMIT 1", guild_id.get() as i64, level_info.level() as i64)
