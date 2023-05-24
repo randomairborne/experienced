bf18e45f37e5ec7a3fe0bcbd47820298e10e6ff1
valkyrie_pilot <valk@randomairborne.dev>
2023-05-23 20:30:27 -0600

    fix montserrat

--- slash/src/cmd_defs/card.rs
+++ slash/src/cmd_defs/card.rs
@@ -55,7 +55,7 @@ pub enum CardCommandEditFont {
     Roboto,
     #[option(name = "JetBrains Mono", value = "JetBrains Mono")]
     JetBrainsMono,
-    #[option(name = "Montserrat Alt1", value = "Montserrat-Alt1")]
+    #[option(name = "Montserrat Alt1", value = "Montserrat Alt1")]
     MontserratAlt1,
 }
 
