fc2746ef216503c203b3684b1788c837b831ef87
valkyrie_pilot <valk@randomairborne.dev>
2023-07-07 14:54:31 -0600

    experienced help improvement

--- xpd-slash/src/help.rs
+++ xpd-slash/src/help.rs
@@ -1,4 +1,6 @@
-use twilight_util::builder::embed::{EmbedBuilder, EmbedFieldBuilder};
+use twilight_util::builder::embed::{
+    EmbedAuthorBuilder, EmbedBuilder, EmbedFieldBuilder, EmbedFooterBuilder, ImageSource,
+};
 
 use crate::XpdSlashResponse;
 
@@ -18,13 +20,21 @@ pub fn help() -> XpdSlashResponse {
     let xp_help = EmbedFieldBuilder::new("/xp", "Commands to manage the bot in this server.")
         .inline()
         .build();
+    let author = EmbedAuthorBuilder::new("Join our support server!")
+        .url("https://valk.sh/discord")
+        .build();
+    let footer = EmbedFooterBuilder::new("Thank you for using experienced.")
+        .icon_url(ImageSource::url("https://xp.valk.sh/favicon.png").unwrap())
+        .build();
     let help_embed = EmbedBuilder::new()
         .color(crate::THEME_COLOR)
+        .author(author)
         .title("Experienced Help")
         .field(help_help)
         .field(rank_help)
         .field(card_help)
         .field(xp_help)
+        .footer(footer)
         .build();
     XpdSlashResponse::new().embeds([help_embed])
 }
