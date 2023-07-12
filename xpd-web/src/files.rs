90cedd9eae3f8422d77115fe1735ac6ee90a89ad
valkyrie_pilot <valk@randomairborne.dev>
2023-07-11 19:28:19 -0600

    Reorganize files

---
+++ xpd-web/src/files.rs
@@ -0,0 +1,60 @@
+use crate::{AppState, Error};
+use axum::{extract::State, response::Html};
+
+#[allow(clippy::unused_async)]
+pub async fn serve_index(State(state): State<AppState>) -> Result<Html<String>, Error> {
+    let mut context = tera::Context::new();
+    context.insert("root_url", &state.root_url);
+    Ok(Html(state.tera.render("index.html", &context)?))
+}
+
+#[allow(clippy::unused_async)]
+pub async fn serve_privacy(State(state): State<AppState>) -> Result<Html<String>, Error> {
+    let mut context = tera::Context::new();
+    context.insert("root_url", &state.root_url);
+    Ok(Html(state.tera.render("privacy.html", &context)?))
+}
+
+#[allow(clippy::unused_async)]
+pub async fn serve_terms(State(state): State<AppState>) -> Result<Html<String>, Error> {
+    let mut context = tera::Context::new();
+    context.insert("root_url", &state.root_url);
+    Ok(Html(state.tera.render("terms.html", &context)?))
+}
+
+#[allow(clippy::unused_async)]
+pub async fn serve_404(State(state): State<AppState>) -> Result<Html<String>, Error> {
+    let mut context = tera::Context::new();
+    context.insert("root_url", &state.root_url);
+    Ok(Html(state.tera.render("404.html", &context)?))
+}
+
+#[allow(clippy::unused_async)]
+pub async fn serve_css() -> ([(&'static str, &'static str); 1], &'static [u8]) {
+    (
+        [("Content-Type", "text/css")],
+        include_bytes!("resources/main.css").as_slice(),
+    )
+}
+
+#[allow(clippy::unused_async)]
+pub async fn serve_font() -> ([(&'static str, &'static str); 2], &'static [u8]) {
+    (
+        [
+            ("Content-Type", "font/woff2"),
+            ("Cache-Control", "max-age=86400"),
+        ],
+        include_bytes!("resources/MontserratAlt1.woff2").as_slice(),
+    )
+}
+
+#[allow(clippy::unused_async)]
+pub async fn serve_icon() -> ([(&'static str, &'static str); 2], &'static [u8]) {
+    (
+        [
+            ("Content-Type", "image/png"),
+            ("Cache-Control", "max-age=86400"),
+        ],
+        include_bytes!("resources/favicon.png").as_slice(),
+    )
+}
