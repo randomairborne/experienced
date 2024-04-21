fee2fbca041db428cbbe736da9753bebb91294bf
valkyrie_pilot <valk@randomairborne.dev>
2024-04-20 22:58:10 -0600

    Fix page build

--- xpd-web/astro.config.mjs
+++ xpd-web/astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from "astro/config";
 
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [mdx()],
-});
+export default defineConfig({});
