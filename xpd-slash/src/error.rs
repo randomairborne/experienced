d06e38ca8edac25a7e35f9229c056088e741cd71
valkyrie_pilot <valk@randomairborne.dev>
2024-05-15 01:48:24 -0600

    Provide less sensitive information on errors

--- xpd-slash/src/error.rs
+++ xpd-slash/src/error.rs
@@ -1,5 +1,37 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("Interaction parser encountered an error!")]
+    Parse(#[from] twilight_interactions::error::ParseError),
+    #[error("Processing task panicked!")]
+    TaskPanicked(#[from] tokio::task::JoinError),
+    #[error("Discord error!")]
+    TwilightHttp(#[from] twilight_http::Error),
+    #[error("Discord validation error!")]
+    InvalidApiRequest(#[from] twilight_validate::request::ValidationError),
+    #[error("HTTP error!")]
+    ReqwestHttp(#[from] reqwest::Error),
+    #[error("Invalid constructed message!")]
+    Validate(#[from] twilight_validate::message::MessageValidationError),
+    #[error("Invalid message attachment!")]
+    ImageSourceAttachment(
+        #[from] twilight_util::builder::embed::image_source::ImageSourceAttachmentError,
+    ),
+    #[error("SVG renderer encountered an error!")]
+    ImageGenerator(#[from] xpd_rank_card::Error),
+    #[error("SQLx encountered an error")]
+    Sqlx(#[from] sqlx::Error),
+    #[error("Command had wrong number of arguments!")]
+    WrongArgumentCount(&'static str),
+    #[error("Rust writeln! returned an error")]
+    Fmt(#[from] std::fmt::Error),
+    #[error("Could not convert string to int")]
+    StrToInt(#[from] std::num::ParseIntError),
+    #[error("CSV error")]
+    Csv(#[from] csv::Error),
+    #[error("I/O error")]
+    Io(#[from] std::io::Error),
+    #[error("Discord API decoding error")]
+    DiscordApiDeserialization(#[from] twilight_http::response::DeserializeBodyError),
     #[error("Discord sent a command that is not known!")]
     UnrecognizedCommand,
     #[error("Discord did not send a user object for the command invoker when it was required!")]
@@ -28,38 +60,6 @@ pub enum Error {
     NotControlUser,
     #[error("There are too many users to import automatically. Please email valk@randomairborne.dev to set up imports for your server.")]
     TooManyUsersForImport,
-    #[error("Interaction parser encountered an error: {0}!")]
-    Parse(#[from] twilight_interactions::error::ParseError),
-    #[error("Processing task panicked: {0}!")]
-    TaskPanicked(#[from] tokio::task::JoinError),
-    #[error("Discord error: {0}!")]
-    TwilightHttp(#[from] twilight_http::Error),
-    #[error("Theoretical discord error: {0}!")]
-    InvalidApiRequest(#[from] twilight_validate::request::ValidationError),
-    #[error("HTTP error: {0}!")]
-    ReqwestHttp(#[from] reqwest::Error),
-    #[error("Invalid constructed message: {0}!")]
-    Validate(#[from] twilight_validate::message::MessageValidationError),
-    #[error("Invalid message attachment: {0}!")]
-    ImageSourceAttachment(
-        #[from] twilight_util::builder::embed::image_source::ImageSourceAttachmentError,
-    ),
-    #[error("SVG renderer encountered an error: {0}!")]
-    ImageGenerator(#[from] xpd_rank_card::Error),
-    #[error("SQLx encountered an error: {0}")]
-    Sqlx(#[from] sqlx::Error),
-    #[error("Command had wrong number of arguments: {0}!")]
-    WrongArgumentCount(&'static str),
-    #[error("Rust writeln! returned an error: {0}")]
-    Fmt(#[from] std::fmt::Error),
-    #[error("Could not convert string to int: {0}")]
-    StrToInt(#[from] std::num::ParseIntError),
-    #[error("CSV error: {0}")]
-    Csv(#[from] csv::Error),
-    #[error("I/O error: {0}")]
-    Io(#[from] std::io::Error),
-    #[error("Discord API decoding error: {0}")]
-    DiscordApiDeserialization(#[from] twilight_http::response::DeserializeBodyError),
     #[error("This page does not exist!")]
     NoUsersForPage,
     #[error("This page does not exist!")]
