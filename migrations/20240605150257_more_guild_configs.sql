4a45a1b4f749b9c20fc6fe15a45ee076071cae0c
valkyrie_pilot <valk@randomairborne.dev>
2024-06-05 11:04:57 -0400

    Add migration for new messages

---
+++ migrations/20240605150257_more_guild_configs.sql
@@ -0,0 +1,9 @@
+-- Add migration script here
+ALTER TABLE guild_configs
+    ADD COLUMN level_up_message VARCHAR(512);
+ALTER TABLE guild_configs
+    ADD COLUMN min_xp_per_message INT2;
+ALTER TABLE guild_configs
+    ADD COLUMN max_xp_per_message INT2;
+ALTER TABLE guild_configs
+    ADD COLUMN message_cooldown INT2;
