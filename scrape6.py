71fd53e1964ca237fd590a236d25e17e0aabc431
valkyrie_pilot <valk@randomairborne.dev>
2024-06-04 21:18:55 -0400

    Dump to JSON and send to stdout

--- scrape6.py
+++ scrape6.py
@@ -7,10 +7,13 @@ mee6_auth = os.getenv("MEE6_AUTH")
 guild = sys.argv[1]
 last_len = 1
 page = 0
-f = open("dump.sql", 'w+')
+
 headers = {
     "Authorization": mee6_auth
 }
+
+players = []
+
 while last_len != 0:
     r = requests.get(f"https://mee6.xyz/api/plugins/levels/leaderboard/{guild}?page={page}", headers=headers).json()
     last_len = len(r["players"])
@@ -18,6 +21,9 @@ while last_len != 0:
     for item in r["players"]:
         id = item["id"]
         xp = item["xp"]
-        f.write(
-            f'INSERT INTO levels (id, xp, guild) VALUES ({id}, {xp}, {guild}) ON CONFLICT (id, guild) DO UPDATE SET xp = EXCLUDED.xp + {xp};\n')
-f.close()
+        players.append({
+            "id": id,
+            "xp": xp
+        })
+
+json.dump(players, sys.stdout)
