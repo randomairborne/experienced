35c6f5a831239dbe915102a647dee62a130467a5
valkyrie_pilot <valk@randomairborne.dev>
2024-06-26 10:32:54 -0600

    Reformat

--- xpd-web/astro.config.mjs
+++ xpd-web/astro.config.mjs
@@ -4,5 +4,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://xp.valk.sh",
-  integrations: [sitemap()]
+  integrations: [sitemap()],
 });
