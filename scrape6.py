48b6701d967e41cb3ac65ec72b9838b7aeb71a6e
valkyrie_pilot <valk@randomairborne.dev>
2024-09-01 15:15:04 -0600

    Improve scrape6.py

--- scrape6.py
+++ scrape6.py
@@ -1,24 +1,36 @@
 import json
 import os
 import sys
-import requests
+
+import urllib.request
 
 mee6_auth = os.getenv("MEE6_AUTH")
+if mee6_auth is None:
+    print("Please set MEE6_AUTH environment variable to the Authorization token from mee6.com")
+    sys.exit(1)
+
+if len(sys.argv) != 2:
+    print("Usage: python3 scrape6.py <url> > export.json")
+    sys.exit(1)
 guild = sys.argv[1]
+
 last_len = 1
 page = 0
 
-headers = {}
-if mee6_auth is not None:
-    headers["Authorization"] = mee6_auth
+headers = {
+    "Authorization": mee6_auth,
+}
 
 players = []
 
 while last_len != 0:
-    r = requests.get(f"https://mee6.xyz/api/plugins/levels/leaderboard/{guild}?page={page}", headers=headers).json()
-    last_len = len(r["players"])
+    url = f"https://mee6.xyz/api/plugins/levels/leaderboard/{guild}?page={page}"
+    req = urllib.request.Request(url, headers=headers)
+    resp = urllib.request.urlopen(req)
+    apiresp = json.loads(resp.read())
+    last_len = len(apiresp["players"])
     page += 1
-    for item in r["players"]:
+    for item in apiresp["players"]:
         id = item["id"]
         xp = item["xp"]
         players.append({
