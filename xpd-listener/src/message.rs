faf474d7e1c731ddc0140b815c34c01a8febed59
valkyrie_pilot <valk@randomairborne.dev>
2024-09-18 21:24:24 -0600

    Remove unused imports

--- xpd-listener/src/message.rs
+++ xpd-listener/src/message.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 
 use rand::Rng;
-use twilight_cache_inmemory::CacheableRole;
 use twilight_model::{
     channel::message::AllowedMentions,
     gateway::payload::incoming::MessageCreate,
