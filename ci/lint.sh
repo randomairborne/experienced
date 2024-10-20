40155cba83755e5a3f204ac9988f3afbe4752388
valkyrie_pilot <valk@randomairborne.dev>
2024-10-20 17:12:52 -0600

    Always disable sqlx online

--- ci/lint.sh
+++ ci/lint.sh
@@ -5,6 +5,6 @@ cargo +nightly fmt --check --all
 echo "Checking web formatting..."
 npm run prettier-check
 echo "Checking build..."
-SQLX_OFFLINE=true cargo +nightly clippy --all -- -D warnings
+cargo +nightly clippy --all -- -D warnings
 echo "Running tests..."
 cargo +nightly test --all
