d0006a8054fac3523a88353f338322e28751600b
valkyrie_pilot <valk@randomairborne.dev>
2024-10-20 14:47:13 -0600

    add ellipse in ci

--- ci/lint.sh
+++ ci/lint.sh
@@ -6,5 +6,5 @@ echo "Checking web formatting..."
 npm run prettier-check
 echo "Checking build..."
 cargo +nightly clippy --all -- -D warnings
-echo "Running tests"
+echo "Running tests..."
 cargo +nightly test --all
