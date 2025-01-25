9792afb08b14b9663e33e8f21cbc198607204959
valkyrie_pilot <valk@randomairborne.dev>
2025-01-24 22:30:51 -0700

    Fix the very stupid wrong interaction response type

--- xpd-slash/src/help.rs
+++ xpd-slash/src/help.rs
@@ -6,5 +6,5 @@ pub fn help() -> XpdInteractionResponse {
     const HELP_MESSAGE: &str = "Visit [the docs](<https://xp.valk.sh/docs/>) or [join the discord](<https://valk.sh/discord>)";
     XpdInteractionData::with_embed_text(HELP_MESSAGE)
         .ephemeral(true)
-        .into_interaction_response(InteractionResponseType::DeferredChannelMessageWithSource)
+        .into_interaction_response(InteractionResponseType::ChannelMessageWithSource)
 }
