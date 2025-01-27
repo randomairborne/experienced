d06a2758acbc9b307c1584f87a4e0176a69c0edb
valkyrie_pilot <valk@randomairborne.dev>
2025-01-27 11:05:28 -0700

    More database tests

--- xpd-database/src/test.rs
+++ xpd-database/src/test.rs
@@ -2,6 +2,7 @@ use sqlx::PgPool;
 use twilight_model::id::Id;
 
 use crate::*;
+
 #[sqlx::test(migrations = "../migrations/")]
 async fn find_deletes_returns_correctly(db: PgPool) -> Result<(), Box<dyn std::error::Error>> {
     // Added 31 days ago, should be included
@@ -31,4 +32,119 @@ async fn find_deletes_returns_correctly(db: PgPool) -> Result<(), Box<dyn std::e
     assert!(cleanups.contains(&Id::new(1)));
     assert!(!cleanups.contains(&Id::new(2)));
     Ok(())
+}
+
+#[sqlx::test(migrations = "../migrations/")]
+async fn audit_log_refetch(db: PgPool) -> Result<(), Box<dyn std::error::Error>> {
+    let original_event = AuditLogEvent {
+        guild_id: Id::new(1),
+        user_id: Id::new(2),
+        moderator: Id::new(3),
+        timestamp: 50,
+        previous: 100,
+        delta: -100,
+        reset: true,
+        set: false,
+    };
+    add_audit_log_event(&db, original_event).await?;
+    let roundtripped_event = get_audit_log_events(&db, Id::new(1), None, None).await?;
+    assert_eq!(roundtripped_event, &[original_event]);
+    Ok(())
+}
+
+#[sqlx::test(migrations = "../migrations/")]
+async fn audit_log_multi(db: PgPool) -> Result<(), Box<dyn std::error::Error>> {
+    let original_events = vec![
+        AuditLogEvent {
+            guild_id: Id::new(1),
+            user_id: Id::new(2),
+            moderator: Id::new(3),
+            timestamp: 50,
+            previous: 100,
+            delta: -100,
+            reset: true,
+            set: false,
+        },
+        AuditLogEvent {
+            guild_id: Id::new(1),
+            user_id: Id::new(4),
+            moderator: Id::new(5),
+            timestamp: 591,
+            previous: 15,
+            delta: 50,
+            reset: false,
+            set: true,
+        },
+    ];
+    for event in &original_events {
+        add_audit_log_event(&db, *event).await?;
+    }
+    let roundtripped_events = get_audit_log_events(&db, Id::new(1), None, None).await?;
+
+    assert_eq!(
+        roundtripped_events.sorted_by_timestamp(),
+        original_events.sorted_by_timestamp()
+    );
+    Ok(())
+}
+
+#[sqlx::test(migrations = "../migrations/")]
+async fn audit_logs_deleted_guild(db: PgPool) -> Result<(), Box<dyn std::error::Error>> {
+    let original_events = vec![
+        AuditLogEvent {
+            guild_id: Id::new(1),
+            user_id: Id::new(2),
+            moderator: Id::new(3),
+            timestamp: 50,
+            previous: 100,
+            delta: -100,
+            reset: false,
+            set: false,
+        },
+        AuditLogEvent {
+            guild_id: Id::new(1),
+            user_id: Id::new(4),
+            moderator: Id::new(5),
+            timestamp: 591,
+            previous: 15,
+            delta: 50,
+            reset: false,
+            set: false,
+        },
+        AuditLogEvent {
+            guild_id: Id::new(2),
+            user_id: Id::new(4),
+            moderator: Id::new(5),
+            timestamp: 595,
+            previous: 100,
+            delta: 50,
+            reset: false,
+            set: true,
+        },
+    ];
+    for event in &original_events {
+        add_audit_log_event(&db, *event).await?;
+    }
+    delete_audit_log_events_guild(&db, Id::new(1)).await?;
+
+    let should_be_nonexistent = get_audit_log_events(&db, Id::new(1), None, None).await?;
+    let roundtripped_events = get_audit_log_events(&db, Id::new(2), None, None).await?;
+
+    assert!(should_be_nonexistent.is_empty());
+    assert_eq!(
+        roundtripped_events.sorted_by_timestamp(),
+        vec![original_events[2]]
+    );
+    Ok(())
+}
+
+trait SortedByTimestamp {
+    fn sorted_by_timestamp(self) -> Self;
+}
+
+impl SortedByTimestamp for Vec<AuditLogEvent> {
+    fn sorted_by_timestamp(mut self) -> Self {
+        self.sort_by(|a, b| a.timestamp.cmp(&b.timestamp));
+        self
+    }
 }
