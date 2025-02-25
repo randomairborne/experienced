99f91c2e552fc831b7a0c290c6a3289411f3eb20
valkyrie_pilot <valk@randomairborne.dev>
2025-02-24 17:27:27 -0700

    Add migration for new audit log table

---
+++ migrations/20250224222943_audit_log_types.sql
@@ -0,0 +1,25 @@
+-- Add migration script here
+CREATE TABLE new_audit_logs (
+    guild INT8 NOT NULL,
+    target INT8 NOT NULL,
+    moderator INT8 NOT NULL,
+    timestamp INT8 NOT NULL,
+    previous INT8 NOT NULL,
+    delta INT8 NOT NULL,
+    kind INT8 NOT NULL,
+);
+CREATE TABLE new_audit_logs AS
+SELECT guild_id as guild, user_id as target, moderator, timestamp, previous, delta,
+CASE
+    /* set */
+    WHEN set THEN 2
+    /* reset */
+    WHEN reset THEN 1
+    /* add/sub */
+    ELSE 0
+END as kind
+FROM audit_logs;
+
+CREATE INDEX ON new_audit_logs(guild);
+DROP TABLE audit_logs;
+ALTER TABLE new_audit_logs RENAME TO audit_logs;
