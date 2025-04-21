01ee025241db8d6e775f710d92e96c38d08a6843
valkyrie_pilot <valk@randomairborne.dev>
2025-04-21 13:56:07 -0600

    Add bot version into help

--- xpd-slash/src/help.rs
+++ xpd-slash/src/help.rs
@@ -1,10 +1,14 @@
 use twilight_model::http::interaction::InteractionResponseType;
+use xpd_common::{CURRENT_GIT_REV_COUNT, CURRENT_GIT_SHA};
 
 use crate::{XpdInteractionData, response::XpdInteractionResponse};
 
 pub fn help() -> XpdInteractionResponse {
-    const HELP_MESSAGE: &str = "Visit [the docs](<https://xp.valk.sh/docs/>) or [join the discord](<https://valk.sh/discord>)";
-    XpdInteractionData::with_embed_text(HELP_MESSAGE)
+    const HELP_MESSAGE: &str = "Visit [the docs](<https://xp.valk.sh/docs/>) or [join the discord](<https://valk.sh/discord>) to learn how to use Experienced!";
+    let message = format!(
+        "{HELP_MESSAGE}\n\nBot git revision `{CURRENT_GIT_SHA}`, commit number {CURRENT_GIT_REV_COUNT}"
+    );
+    XpdInteractionData::with_embed_text(message)
         .ephemeral(true)
         .into_interaction_response(InteractionResponseType::ChannelMessageWithSource)
 }
