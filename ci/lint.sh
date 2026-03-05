c7f3d575ec3d351ad5604b4b876b5b431507a94b
valkyrie_pilot <valk@randomairborne.dev>
2026-03-05 15:23:58 -0600

    Move CI to script

--- ci/lint.sh
+++ ci/lint.sh
@@ -6,5 +6,20 @@ echo "Checking web formatting..."
 npm run prettier-check
 echo "Checking build..."
 cargo +nightly clippy --all -- -D warnings
+echo "Creating test database..."
+docker stop sqlx-postgres
+docker rm sqlx-postgres
+docker run -d --name sqlx-postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 \
+  --health-cmd pg_isready --health-interval 10s --health-timeout 5s --health-retries 5 postgres
+DATABASE_URL=postgres://postgres:postgres@localhost:5432/postgres
+until docker inspect --format='{{json .State.Health.Status}}' sqlx-postgres | grep "healthy"
+do
+echo "Waiting for database to become healthy..."
+sleep 0.3
+done
 echo "Running tests..."
 cargo +nightly test --all
+echo "Cleaning up..."
+docker stop sqlx-postgres
+docker rm sqlx-postgres
+echo "Done!"
--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-docker run -d --name sqlx-postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres
+docker run -d --name sqlx-postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 \
+  --health-cmd pg_isready --health-interval 10s --health-timeout 5s --health-retries 5 postgres
 DATABASE_URL=postgres://postgres:postgres@localhost:5432/postgres
+until docker inspect --format='{{json .State.Health.Status}}' sqlx-postgres | grep "healthy"
+do
+echo "Waiting for database to become healthy..."
+sleep 0.3
+done
 cargo sqlx mig run
 cargo sqlx prepare --workspace -- --all-features --all-targets
 docker stop sqlx-postgres
